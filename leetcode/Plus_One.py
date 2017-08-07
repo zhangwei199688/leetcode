b27085b3d05f3b3aad512b21489979c4a55e282e
Zhangv <442478310@qq.com>
2017-08-07 21:44:57 +0800

    2017/8/7

--- leetcode/Plus_One.py
+++ leetcode/Plus_One.py
@@ -8,16 +8,21 @@ The digits are stored such that the most significant digit is at the head of the
 
 '''
 
+
 class Solution(object):
     def plusOne(self, digits):
-        digits[-1]+=1
-        for i in range(1,len(digits)+1):
-            if i!=len(digits):
-                if digits[-i]>9:
-                    digits[-i]=0
-                    digits[-i-1]+=1
-                else: return digits
-        if digits[0]>9:
-            digits[0]=0
-            digits=[1]+digits
+        digits[-1] += 1
+        for i in range(1, len(digits) + 1):
+            if i != len(digits):
+                if digits[-i] > 9:
+                    digits[-i] = 0
+                    digits[-i - 1] += 1
+                else:
+                    return digits
+        if digits[0] > 9:
+            digits[0] = 0
+            digits.insert(0, 1)
         return digits
+
+
+#Your runtime beats 95.00 % of python submissions.
