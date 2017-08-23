bccf3f0000798b99baf85eee01490b28ddeb0d10
Zhangv <442478310@qq.com>
2017-08-23 08:14:51 +0800

    2017/8/23

---
+++ leetcode/Buy_and_sell_stock_2.py
@@ -0,0 +1,15 @@
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+'''
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        profit = 0
+        length = len(prices)
+        for i in range(0,length-1):
+            if prices[i+1] > prices[i]:
+                profit += prices[i+1] - prices[i]
+        return profit
