b366ea356c20a06b8af30b2e15b19e8ced34c330
Zhangv <442478310@qq.com>
2017-09-05 20:07:05 +0800

    2017/9/5

---
+++ leetcode/Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,36 @@
+'''
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param two ListNodes
+    # @return the intersected ListNode
+    def getIntersectionNode(self, headA, headB):
+        if headA is None or headB is None:
+            return None
+
+        pa = headA # 2 pointers
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal,
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
